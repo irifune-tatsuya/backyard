f7e5b13a7a09445511b4bacb6f4c2fbba46bd1b8
irifune-tatsuya <irifune.tatsuya@gmail.com>
2020-05-12 20:21:33 +0900

    update_files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'branches#index'
   resources :branches, only: [:new, :create] do
-    resources :employees, only: [:index]
+    resources :employees, only: :index
   end
   resources :employees, only: [:new, :create]
 end
